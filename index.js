202cedb686385bf96f96e4ba2edfd802d312d8cd
arymaulana <ary.maulana27@gmail.com>
2023-08-02 11:35:22 +0800

    fix: return error

--- index.js
+++ index.js
@@ -115,20 +115,20 @@ app.post("/bot-register", (req, res) => {
 
     transporter.sendMail(mailOptions, (err, data) => {
       if (err) {
-        res.status(500).json({ message: "Internal Error" });
+        res.status(500).json({ message: err.message });
       } else {
         res.status(200).json({ message: "Email sent successfully" });
       }
     });
 
-    res.status(200).json({ message: "Email sent successfully" });
   } catch (error) {
-    console.log(error);
+    res.status(500).json({ message: "Internal Error :" + error.message });
   }
+  return
 });
 
 app.get("/", (req, res) => {
-  res.status(404).json(`Not Found`);
+  return res.status(404).json(`Not Found`);
 });
 
 app.listen(process.env.PORT, () => {
